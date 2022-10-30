522904d422287a93b79349d6697f35826b643bd5
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-10-30 18:11:40 -0500

    :recycle: add tailwindcss className for initial test

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 
 // Delete me
 export const Thing = () => {
-  return <div>Welcome to your first test package.</div>;
+  return <div className="text-2xl text-gray-800"><span className="hover:text-blue-700">Welcome</span> to your first test package.</div>;
 };
