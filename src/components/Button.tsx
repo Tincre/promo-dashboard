d48180af509c652263f73507050652e280399509
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-10-31 22:43:58 -0500

    :recycle: add default styling className

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,7 @@
-import {MouseEventHandler, } from "react";
+import { MouseEventHandler } from 'react';
+
+const baseClassName =
+  'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600';
 
 export function Button({
   className,
@@ -10,7 +13,15 @@ export function Button({
   children?: string;
 }) {
   return (
-    <button className={className} type="button" onClick={onClick}>
+    <button
+      className={
+        typeof className !== 'undefined'
+          ? baseClassName + className
+          : baseClassName
+      }
+      type="button"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
