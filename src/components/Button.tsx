2013ac0bf0136000780f667d75ebec680aaa7275
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-10-31 22:35:08 -0500

    :recycle: :bug: add missing onClick prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,16 @@
+import {MouseEventHandler, } from "react";
+
 export function Button({
   className,
   onClick,
   children,
 }: {
   className?: string;
-  onClick?: Function;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   children?: string;
 }) {
   return (
-    <button className={className} type="button">
+    <button className={className} type="button" onClick={onClick}>
       {children}
     </button>
   );
