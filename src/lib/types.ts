e09886916e6e94226c644b217e992b17c29f5f71
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-10-31 22:57:23 -0500

    :sparkles: :label: add specific types module

---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+export interface CampaignData {
+  pid?: string;
+  adTitle?: string;
+  budget?: string | number;
+  description?: string;
+  target?: string;
+  adCopy?: string;
+  adCallToAction?: string;
+  buttonText?: string;
+  isActive?: boolean;
+}
