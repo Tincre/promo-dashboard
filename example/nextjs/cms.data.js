461a7f8bb73bf01fe6a553ce71987ebd9c5d4700
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-01 20:16:34 -0500

    :recycle: add custom stats mocked data for campaigns

--- example/nextjs/cms.data.js
+++ example/nextjs/cms.data.js
@@ -4,7 +4,7 @@ import {
   UsersIcon,
   CurrencyDollarIcon,
   VideoCameraIcon,
-} from '@heroicons/react/24/outline'
+} from '@heroicons/react/24/outline';
 
 export const stats = [
   {
@@ -50,12 +50,12 @@ export const stats = [
   {
     id: 6,
     name: 'CPC',
-    stat: '$.42',
+    stat: '$.32',
     icon: CursorArrowRaysIcon,
     change: '.03',
     changeType: 'increase',
   },
-]
+];
 export const campaignStubData = [
   {
     name: 'Jane Cooper',
@@ -75,7 +75,56 @@ export const campaignStubData = [
     telephone: '+1-202-555-0170',
     imageUrl:
       'https://res.cloudinary.com/tincre/image/upload/v1666907624/tailwindui-salient/taxpal-og_xka3ix_q4ulfc.webp',
-    stats: stats,
+    stats: [
+      {
+        id: 1,
+        name: 'Spend',
+        stat: '1283',
+        icon: CurrencyDollarIcon,
+        change: '122',
+        changeType: 'increase',
+      },
+      {
+        id: 2,
+        name: 'Clicks',
+        stat: '1092',
+        icon: CursorArrowRaysIcon,
+        change: '202',
+        changeType: 'increase',
+      },
+      {
+        id: 3,
+        name: 'Views',
+        stat: '12909',
+        icon: VideoCameraIcon,
+        change: '1012',
+        changeType: 'increase',
+      },
+      {
+        id: 4,
+        name: 'CPM',
+        stat: '58.16%',
+        icon: EnvelopeOpenIcon,
+        change: '5.4%',
+        changeType: 'increase',
+      },
+      {
+        id: 5,
+        name: 'CTR',
+        stat: '4.57%',
+        icon: UsersIcon,
+        change: '.2%',
+        changeType: 'decrease',
+      },
+      {
+        id: 6,
+        name: 'CPC',
+        stat: '$.03',
+        icon: CursorArrowRaysIcon,
+        change: '.01',
+        changeType: 'decrease',
+      },
+    ],
   },
   // More people...
   {
@@ -96,7 +145,56 @@ export const campaignStubData = [
     telephone: '+1-202-555-0170',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    stats: stats,
+    stats: [
+      {
+        id: 1,
+        name: 'Spend',
+        stat: '2892',
+        icon: CurrencyDollarIcon,
+        change: '74',
+        changeType: 'increase',
+      },
+      {
+        id: 2,
+        name: 'Clicks',
+        stat: '11593',
+        icon: CursorArrowRaysIcon,
+        change: '121',
+        changeType: 'increase',
+      },
+      {
+        id: 3,
+        name: 'Views',
+        stat: '312909',
+        icon: VideoCameraIcon,
+        change: '3491',
+        changeType: 'increase',
+      },
+      {
+        id: 4,
+        name: 'CPM',
+        stat: '$4.16',
+        icon: EnvelopeOpenIcon,
+        change: '5.4%',
+        changeType: 'decrease',
+      },
+      {
+        id: 5,
+        name: 'CTR',
+        stat: '6.71%',
+        icon: UsersIcon,
+        change: '.2%',
+        changeType: 'increase',
+      },
+      {
+        id: 6,
+        name: 'CPC',
+        stat: '$.42',
+        icon: CursorArrowRaysIcon,
+        change: '.01',
+        changeType: 'decrease',
+      },
+    ],
   },
   {
     name: 'Jane Cooper',
@@ -115,9 +213,57 @@ export const campaignStubData = [
     buttonText: 'Sign up',
     telephone: '+1-202-555-0170',
     imageUrl:
-
       'https://res.cloudinary.com/tincre/image/upload/v1666907673/tailwindui-salient/background-faqs_nugiew_m0skue.webp',
-    stats: stats,
+    stats: [
+      {
+        id: 1,
+        name: 'Spend',
+        stat: '6',
+        icon: CurrencyDollarIcon,
+        change: '6',
+        changeType: 'increase',
+      },
+      {
+        id: 2,
+        name: 'Clicks',
+        stat: '0',
+        icon: CursorArrowRaysIcon,
+        change: '0',
+        changeType: 'increase',
+      },
+      {
+        id: 3,
+        name: 'Views',
+        stat: '16',
+        icon: VideoCameraIcon,
+        change: '16',
+        changeType: 'increase',
+      },
+      {
+        id: 4,
+        name: 'CPM',
+        stat: '$18.16',
+        icon: EnvelopeOpenIcon,
+        change: '10.0%',
+        changeType: 'increase',
+      },
+      {
+        id: 5,
+        name: 'CTR',
+        stat: '1.84%',
+        icon: UsersIcon,
+        change: '.5%',
+        changeType: 'increase',
+      },
+      {
+        id: 6,
+        name: 'CPC',
+        stat: '$.14',
+        icon: CursorArrowRaysIcon,
+        change: '.02',
+        changeType: 'decrease',
+      },
+    ],
   },
   {
     name: 'Jane Cooper',
@@ -137,7 +283,56 @@ export const campaignStubData = [
     telephone: '+1-202-555-0170',
     imageUrl:
       'https://res.cloudinary.com/tincre/image/upload/v1666907587/tailwindui-salient/screenshots/vat-returns_uhkad6_c8tze2.webp',
-    stats: stats,
+    stats: [
+      {
+        id: 1,
+        name: 'Spend',
+        stat: '1283',
+        icon: CurrencyDollarIcon,
+        change: '122',
+        changeType: 'increase',
+      },
+      {
+        id: 2,
+        name: 'Clicks',
+        stat: '1092',
+        icon: CursorArrowRaysIcon,
+        change: '202',
+        changeType: 'increase',
+      },
+      {
+        id: 3,
+        name: 'Views',
+        stat: '12909',
+        icon: VideoCameraIcon,
+        change: '1012',
+        changeType: 'increase',
+      },
+      {
+        id: 4,
+        name: 'CPM',
+        stat: '$5.10',
+        icon: EnvelopeOpenIcon,
+        change: '3.1%',
+        changeType: 'increase',
+      },
+      {
+        id: 5,
+        name: 'CTR',
+        stat: '3.33%',
+        icon: UsersIcon,
+        change: '.3%',
+        changeType: 'decrease',
+      },
+      {
+        id: 6,
+        name: 'CPC',
+        stat: '$1.25',
+        icon: CursorArrowRaysIcon,
+        change: '.03',
+        changeType: 'decrease',
+      },
+    ],
   },
   {
     name: 'Jane Cooper',
@@ -158,7 +353,56 @@ export const campaignStubData = [
     telephone: '+1-202-555-0170',
     imageUrl:
       'https://res.cloudinary.com/tincre/image/upload/v1666907624/tailwindui-salient/taxpal-og_xka3ix_q4ulfc.webp',
-    stats: stats,
+    stats: [
+      {
+        id: 1,
+        name: 'Spend',
+        stat: '8725',
+        icon: CurrencyDollarIcon,
+        change: '219',
+        changeType: 'increase',
+      },
+      {
+        id: 2,
+        name: 'Clicks',
+        stat: '21944',
+        icon: CursorArrowRaysIcon,
+        change: '421',
+        changeType: 'increase',
+      },
+      {
+        id: 3,
+        name: 'Views',
+        stat: '62909',
+        icon: VideoCameraIcon,
+        change: '5042',
+        changeType: 'increase',
+      },
+      {
+        id: 4,
+        name: 'CPM',
+        stat: '$8.16',
+        icon: EnvelopeOpenIcon,
+        change: '5.4%',
+        changeType: 'decrease',
+      },
+      {
+        id: 5,
+        name: 'CTR',
+        stat: '4.17%',
+        icon: UsersIcon,
+        change: '.4%',
+        changeType: 'decrease',
+      },
+      {
+        id: 6,
+        name: 'CPC',
+        stat: '$.51',
+        icon: CursorArrowRaysIcon,
+        change: '.13',
+        changeType: 'increase',
+      },
+    ],
   },
   {
     name: 'Jane Cooper',
@@ -179,6 +423,55 @@ export const campaignStubData = [
     telephone: '+1-202-555-0170',
     imageUrl:
       'https://res.cloudinary.com/tincre/image/upload/v1666907624/tailwindui-salient/taxpal-og_xka3ix_q4ulfc.webp',
-    stats: stats,
+    stats: [
+      {
+        id: 1,
+        name: 'Spend',
+        stat: '250',
+        icon: CurrencyDollarIcon,
+        change: '9',
+        changeType: 'increase',
+      },
+      {
+        id: 2,
+        name: 'Clicks',
+        stat: '451',
+        icon: CursorArrowRaysIcon,
+        change: '20',
+        changeType: 'increase',
+      },
+      {
+        id: 3,
+        name: 'Views',
+        stat: '0',
+        icon: VideoCameraIcon,
+        change: '0',
+        changeType: 'increase',
+      },
+      {
+        id: 4,
+        name: 'CPM',
+        stat: '$5.42',
+        icon: EnvelopeOpenIcon,
+        change: '1.2%',
+        changeType: 'increase',
+      },
+      {
+        id: 5,
+        name: 'CTR',
+        stat: '3.57%',
+        icon: UsersIcon,
+        change: '.3%',
+        changeType: 'decrease',
+      },
+      {
+        id: 6,
+        name: 'CPC',
+        stat: '$.16',
+        icon: CursorArrowRaysIcon,
+        change: '.03',
+        changeType: 'increase',
+      },
+    ],
   },
-]
+];
