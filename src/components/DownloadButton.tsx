fbacd134443681f6022041c829470bc3d7b2449d
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-03 16:01:02 -0500

    :sparkles: add DownloadAllCampaignsButton component

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -6,6 +6,34 @@ import { now } from '../lib/date';
 const baseClassName =
   'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600';
 
+export function DownloadAllCampaignsButton({
+  campaignsData,
+}: {
+  campaignsData: CampaignData[];
+}) {
+  const [localCampaignsData, setLocalCampaignsData] = useState<object>({});
+  useEffect(() => {
+    console.debug(
+      `DownloadCampaignButton component data: ${JSON.stringify(campaignsData)}`
+    );
+    setLocalCampaignsData(campaignsData);
+  }, [setLocalCampaignsData, campaignsData]);
+  /* @ts-ignore */
+  const filename = `all-campaigns_${now()}.csv`;
+
+  return (
+    <div className="mt-12">
+      <CsvDownload
+        className={baseClassName}
+        data={[localCampaignsData]}
+        style={{ display: 'inline' }}
+        filename={filename}
+      >
+        Download Data
+      </CsvDownload>
+    </div>
+  );
+}
 export function DownloadCampaignButton({
   campaignData,
 }: {
