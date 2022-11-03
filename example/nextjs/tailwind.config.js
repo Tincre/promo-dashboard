fa4f722e9c3c883e5180c8b9438d70039d4ca8d9
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-03 11:35:50 -0500

    :recycle: fix and add additional content path for promo-button lib

--- example/nextjs/tailwind.config.js
+++ example/nextjs/tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@tincre/promo-button/dist/*.{js,ts,jsx,tsx}',
+    './node_modules/@tincre/promo-dashboard/*.{js,ts,jsx,tsx}',
+    './node_modules/@tincre/promo-dashboard/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
