89a114b1eddfdce3bb8f3706ce7259311c9f31c0
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-03 14:59:52 -0500

    :sparkles: add download component

---
+++ src/components/DownloadButton.tsx
@@ -0,0 +1,33 @@
+import { CampaignData } from '../lib/types';
+import { useState, useEffect } from 'react';
+import CsvDownload from 'react-json-to-csv';
+import { now } from '../lib/date';
+
+const baseClassName =
+  'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600';
+
+export function DownloadCampaignButton({
+  campaignData,
+}: {
+  campaignData: CampaignData;
+}) {
+  const [localCampaignData, setLocalCampaignData] = useState<object>({});
+  useEffect(() => {
+    console.debug(
+      `DownloadCampaignButton component data: ${JSON.stringify(campaignData)}`
+    );
+    setLocalCampaignData(campaignData);
+  }, [setLocalCampaignData, campaignData]);
+  /* @ts-ignore */
+  const filename = `${localCampaignData.pid}_${now()}.csv`;
+  return (
+    <CsvDownload
+      className={baseClassName}
+      data={[localCampaignData]}
+      style={{ display: 'inline' }}
+      filename={filename}
+    >
+      Download
+    </CsvDownload>
+  );
+}
