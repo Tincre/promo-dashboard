3e95edbc23c9c397d5436357853e7fce5506c011
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-03 06:31:13 -0500

    :sparkles: add generateFloats and generateCPM functions

--- example/nextjs/lib/gen-data.ts
+++ example/nextjs/lib/gen-data.ts
@@ -1,10 +1,17 @@
 import { rando } from '@nastyox/rando.js';
 
-export function generateViews(start: number, end: number, length: number = 13) {
-  const result = generateIntegers(start, end, length);
-  console.debug(`generateClicks: ${JSON.stringify(result)}`);
+export function generateCPM(start: number, end: number, length: number = 13) {
+  const result = generateFloats(start, end, length);
+  console.debug(`generateFloats: ${JSON.stringify(result)}`);
   return result;
 }
+
+export function generateViews(start: number, end: number, length: number = 13) {
+  const result = generateIntegers(start, end, length);
+  console.debug(`generateViews: ${JSON.stringify(result)}`);
+  return result;
+}
+
 export function generateClicks(
   start: number,
   end: number,
@@ -35,6 +42,25 @@ function generateIntegers(start: number, end: number, length: number = 13) {
     } else {
       const target = interval * i;
       result[i] = rando(lastVal, lastVal + target);
+    }
+  }
+  return result;
+}
+
+function generateFloats(start: number, end: number, length: number = 13) {
+  // generate sequence array in 10s
+  const interval: number = (end - start) / length;
+  const upperIndexBound: number = length - 2;
+  let result: number[] = [length, 0];
+  let lastVal: number = start;
+  for (let i = 0; i < length; i += 1) {
+    if (i < 1) {
+      result[i] = start;
+    } else if (i > upperIndexBound) {
+      result[i] = end;
+    } else {
+      const target = interval * i;
+      result[i] = rando(lastVal, lastVal + target, 'float');
     }
   }
   return result;
