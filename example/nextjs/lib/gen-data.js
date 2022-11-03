de477a8a3b61079102fad203ae24afc452355c70
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-02 20:47:39 -0500

    :recycle: apply generateClicks for click data generation

--- example/nextjs/lib/gen-data.js
+++ example/nextjs/lib/gen-data.js
@@ -1,17 +1,11 @@
 import { rando } from '@nastyox/rando.js';
-
-export function generateSpend(start, end, length = 10) {
+function generateIntegers(start, end, length = 13) {
   // generate sequence array in 10s
   const interval = Math.round((end - start) / length);
   const upperIndexBound = length - 2;
-  console.log(
-    `generateSpend setup: 10 $${interval} segments between ${start} and ${end} ${upperIndexBound}`
-  );
-
   let result = new Array(length, 0);
   let lastVal;
   for (let i = 0; i < length; i += 1) {
-    console.log(`generating index ${i} between ${start} and ${end}`);
     if (i < 1) {
       result[i] = start;
       lastVal = start;
@@ -22,6 +16,15 @@ export function generateSpend(start, end, length = 10) {
       result[i] = rando(lastVal, lastVal + target);
     }
   }
+  return result;
+}
+export function generateClicks(start, end, length = 13) {
+  const result = generateIntegers(start, end, length);
+  console.log(`generateClicks: ${JSON.stringify(result)}`);
+  return result;
+}
+export function generateSpend(start, end, length = 10) {
+  const result = generateIntegers(start, end, length);
   console.log(`generateSpend: ${JSON.stringify(result)}`);
   return result;
 }
