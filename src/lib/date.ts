6ccb5da4f139bdb1241271092ddb1c2f4f8ed41b
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-26 17:12:37 -0500

    :recycle: remove superfluous logging

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,7 +1,4 @@
 export function now() {
   const event = new Date();
-  console.log(event.toString());
-  const date = event.toISOString().slice(0, 10); // don't include time
-  console.debug(`Generating date ${date}`);
-  return date;
+  return event.toISOString().slice(0, 10); // don't include time
 }
