650151e464337390df9700b5dee3e2081ea4b193
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-26 21:04:13 -0500

    :white_check_mark: add download button components unit tests

---
+++ test/components/DownloadButton.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import {
+  DownloadAllCampaignsButton,
+  DownloadCampaignButton,
+} from '../../src/components/DownloadButton';
+import { campaignStubData } from '../cms.data';
+
+Object.defineProperty(URL, 'createObjectURL', {
+  writable: true,
+  value: jest.fn()
+})
+describe('DownloadCampaignButton', () => {
+  it('renders without crashing', () => {
+    render(<DownloadCampaignButton campaignData={campaignStubData[0]} />);
+    const downloadButton = screen.getByRole('button');
+    expect(downloadButton).toBeDefined();
+    fireEvent.click(downloadButton);
+  });
+});
+
+describe('DownloadAllCampaignButton', () => {
+  it('renders without crashing', () => {
+    render(<DownloadAllCampaignsButton campaignsData={campaignStubData} />);
+    const downloadButton = screen.getByRole('button');
+    expect(downloadButton).toBeDefined();
+    fireEvent.click(downloadButton);
+  });
+});
