23b2af65eeb96901df61e4cf7c9e88dac48035a6
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-11-27 20:47:44 -0500

    :sparkles: add numActiveCampaigns exported method #6

--- src/lib/sort.ts
+++ src/lib/sort.ts
@@ -6,3 +6,11 @@ export function sortCampaignDataOnIsActive(data: CampaignData[]) {
   );
   return newArray;
 }
+
+export const numActiveCampaigns = (sortedCampaignsData: CampaignData[]) => {
+  let activeCampaigns = 0;
+  sortedCampaignsData.map((campaign) =>
+    campaign.isActive ? (activeCampaigns += 1) : null
+  );
+  return activeCampaigns;
+};
