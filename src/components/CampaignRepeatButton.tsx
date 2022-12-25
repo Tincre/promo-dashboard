09cbd6a233e3630595789eb9944039b736b0355a
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-12-24 19:18:30 -0500

    :page_facing_up: add MPL 2.0 license notice

--- src/components/CampaignRepeatButton.tsx
+++ src/components/CampaignRepeatButton.tsx
@@ -1,3 +1,9 @@
+/* Copyright Tincre (Musicfox, Inc)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { MouseEvent, ReactNode } from 'react';
 import { CampaignData } from '../lib/types';
 import { ArrowPathIcon } from '@heroicons/react/20/solid';
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,10 @@
+/* Copyright Tincre (Musicfox, Inc)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import { useState, useEffect, MouseEvent } from 'react';
 import { Profile } from './components/Profile';
 import { CampaignDetail } from './components/CampaignDetail';
