106f78e9c3746d342f06aaa6fb3c9fb436528073
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-12-24 19:18:27 -0500

    :fire: :page_facing_up: remove unused imports + add license

--- src/components/StatsHighlights.tsx
+++ src/components/StatsHighlights.tsx
@@ -1,4 +1,9 @@
-import { useState, useEffect } from 'react';
+/* Copyright Tincre (Musicfox, Inc)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid';
 
 /* @ts-ignore */
