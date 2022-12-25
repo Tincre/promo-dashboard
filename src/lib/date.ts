7e6e25d90788062ff9e022e44ef73c9089fc14e2
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-12-24 19:19:32 -0500

    :page_facing_up: add MPL 2.0 license notice

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,3 +1,9 @@
+/* Copyright Tincre (Musicfox, Inc)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 export function now() {
   const event = new Date();
   return event.toISOString().slice(0, 10); // don't include time
