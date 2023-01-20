c416ca6a858132e7c4af2e68098a94707e8db5c3
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-01-20 13:20:24 -0600

    :recycle: rename imageUrl to creativeUrls[0]
    - also adds checks for length
    - also adds checks for existence of data and creativeUrls array

--- src/components/CampaignImageChart.tsx
+++ src/components/CampaignImageChart.tsx
@@ -18,7 +18,11 @@ export function CampaignImageChart({
     <div className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2">
       <div className="relative overflow-hidden rounded-lg bg-slate-50 px-4 py-5 shadow sm:px-6 sm:py-6">
         <AdPreviewDetailImage
-          imageUrl={data.imageUrl /* TODO update w/cloudinary array */}
+          creativeUrl={
+            data && data?.creativeUrls && data.creativeUrls.length !== 0
+              ? data.creativeUrls[0]
+              : undefined /* TODO update w/cloudinary array */
+          }
         />
       </div>
       <div className="relative overflow-hidden rounded-lg bg-slate-50 px-4 py-5 pb-0 shadow sm:px-6 sm:py-6">
