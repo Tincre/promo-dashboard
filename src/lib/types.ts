992940a738ed5fc8f5f08964afabd0526d0de614
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-02-22 16:16:00 -0600

    :label: add types for campaign data and downloadable campaign data #9

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,6 +4,53 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+export interface DownloadableCampaignStatsSample
+  extends PromoApiCampaignStatsSample {
+  pid: string;
+}
+export interface PromoApiCampaignStatsSample {
+  updatedTime: string;
+  spend?: number | null;
+  reach?: number | null;
+  views?: number | null;
+  clicks?: number | null;
+  cpc?: number | null;
+  ctr?: number | null;
+  cpm?: number | null;
+  cpv?: number | null;
+}
+export interface CampaignStatsSample {
+  name: string;
+}
+export interface PromoApiCampaignStatsData {
+  totals?: CampaignStatsSample[];
+  google?: CampaignStatsSample[];
+  meta?: CampaignStatsSample[];
+}
+export interface CampaignStatsData {
+  id: number;
+  name: string;
+  stat: string;
+  change: string | number;
+  changeType: string;
+  chartData: {
+    labels: (string | null)[];
+    data: (string | number | null)[];
+  };
+}
+
+export interface DownloadableCampaignStats {
+  updatedTime: (string | null)[];
+  spend: (number | null)[];
+  reach?: (number | null)[];
+  views: (number | null)[];
+  clicks: (number | null)[];
+  cpc: (number | null)[];
+  cpm: (number | null)[];
+  ctr: (number | null)[];
+  cpv: (number | null)[];
+  pid: (string | null)[];
+}
 export interface CampaignData {
   pid?: string;
   email?: string;
@@ -18,7 +65,7 @@ export interface CampaignData {
   isActive?: boolean;
   currency?: string;
   stats?: object[];
-  data?: object[]; // TODO Update with object of object containing meta, google,
+  data?: CampaignStatsData[];
 }
 export interface Settings {
   fullName?: string;
