3db3f066c82b3b069882d7d73b905690a6cbc2bb
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-02-24 10:21:07 -0600

    :sparkles: add component for generating campaign payment link #13

---
+++ src/components/CampaignPaymentButton.tsx
@@ -0,0 +1,45 @@
+/* Copyright Tincre (Musicfox, Inc)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+import { MouseEvent, ReactNode } from 'react';
+import { CampaignData } from '../lib/types';
+import { CreditCardIcon } from '@heroicons/react/20/solid';
+
+export function CampaignPaymentButton({
+  data,
+  handleGeneratePaymentLinkButtonClick,
+  children,
+}: {
+  data: CampaignData;
+  handleGeneratePaymentLinkButtonClick?: (
+    event: MouseEvent<HTMLButtonElement>,
+    data: CampaignData
+  ) => void;
+  children?: ReactNode;
+}) {
+  return (
+    <div className="-ml-px flex w-0 flex-1 promo-dashboard-campaign-repeat-button">
+      <button
+        aria-label={`campaign-${data.pid}-payment-button`}
+        type="button"
+        onClick={(event) =>
+          typeof handleGeneratePaymentLinkButtonClick !== 'undefined'
+            ? handleGeneratePaymentLinkButtonClick(event, data)
+            : console.debug(
+                `promo-dashboard::CampaignPaymentButton::Undefined handleGeneratePaymentLinkButtonClick`
+              )
+        }
+        className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-slate-700 hover:bg-slate-300 hover:text-slate-500 group-hover:text-slate-800"
+      >
+        <CreditCardIcon
+          className="h-5 w-5 text-slate-400 group-hover:text-slate-500"
+          aria-hidden="true"
+        />
+        <span className="ml-3">{children || 'Payment'}</span>
+      </button>
+    </div>
+  );
+}
