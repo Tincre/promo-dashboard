764fdac23f865f10fad387f746761a918e9462c4
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-02-23 22:37:52 -0600

    :fire: remove undefined cc from email string #12

--- src/components/Campaign.tsx
+++ src/components/Campaign.tsx
@@ -12,7 +12,7 @@ import { CampaignSupportButton } from './CampaignSupportButton';
 
 const supportDomain = 'tincre.dev';
 const getSupportLink = (data: any) =>
-  `mailto:team@${supportDomain}?cc=${data.email}&subject=${data.pid}%20-%20Support%20request%20from%20${supportDomain}&body=Hi!%20I%20am%20reaching%20out%20to%20request%20support%20for%20campaign%20${data.pid}.%0A%0AMy%20issue%3A%0A%0A%0A%0ACampaign%20input%20data%3A%0A%20%20Ad%20Title%3A%20${data.adTitle}%0A%20%20Target%20link%3A%20${data.target}%0A%20%20Budget%3A%20${data.budget}%0A%20%20Ad%20Copy%3A%20${data.adCopy}%0A%20%20Call%20to%20action%3A%20${data.callToAction}%0A%20%20Button%20Text%3A%20${data.buttonText}`;
+  `mailto:team@${supportDomain}?subject=${data.pid}%20-%20Support%20request%20from%20${supportDomain}&body=Hi!%20I%20am%20reaching%20out%20to%20request%20support%20for%20campaign%20${data.pid}.%0A%0AMy%20issue%3A%0A%0A%0A%0ACampaign%20input%20data%3A%0A%20%20Ad%20Title%3A%20${data.adTitle}%0A%20%20Target%20link%3A%20${data.target}%0A%20%20Budget%3A%20${data.budget}%0A%20%20Ad%20Copy%3A%20${data.adCopy}%0A%20%20Call%20to%20action%3A%20${data.callToAction}%0A%20%20Button%20Text%3A%20${data.buttonText}`;
 
 export function Campaign({
   data,
