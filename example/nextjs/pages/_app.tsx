03d6b5471fb4a47589122459de1dca932e11850c
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-02-27 16:06:01 -0600

    :recycle: update to properly named promoDashboardTourSteps

--- example/nextjs/pages/_app.tsx
+++ example/nextjs/pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app';
 //import '../../../dist/promo-button.esm.css'; // @tincre/promo-button/bundle.css
 import { TourProvider } from '@reactour/tour';
-import { PromoDashboardTourSteps } from '@tincre/promo-dashboard';
+import { promoDashboardTourSteps } from '@tincre/promo-dashboard';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <TourProvider steps={PromoDashboardTourSteps}>
+    <TourProvider steps={promoDashboardTourSteps}>
       <Component {...pageProps} />
     </TourProvider>
   );
