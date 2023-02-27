103fb32b80ac92c4eaf6e38da119e1f41f58972b
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-02-27 16:02:14 -0600

    :recycle: :construction: add import from promo-dashboard for tour content

--- example/nextjs/pages/_app.tsx
+++ example/nextjs/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from 'next/app';
 //import '../../../dist/promo-button.esm.css'; // @tincre/promo-button/bundle.css
 import { TourProvider } from '@reactour/tour';
+import { PromoDashboardTourSteps } from '@tincre/promo-dashboard';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <TourProvider steps={[{ selector: '#main', content: 'Welcome' }]}>
+    <TourProvider steps={PromoDashboardTourSteps}>
       <Component {...pageProps} />
     </TourProvider>
   );
