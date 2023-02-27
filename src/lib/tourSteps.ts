7695e6691a33414486a6050f35914ca04d5d766d
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-02-27 15:35:29 -0600

    :sparkles: add tourSteps module #14

---
+++ src/lib/tourSteps.ts
@@ -0,0 +1,39 @@
+export const tourSteps = [
+  {
+    selector: '#campaign-0',
+    content:
+      '👋 Welcome to your campaign dashboard. Once you run your first campaign it will show up here.',
+  },
+  {
+    selector: '#campaign-0',
+    content:
+      '🌱 Each campaign you run will show up here and update once daily, or as ad platforms return us data.',
+  },
+  {
+    selector: '#download-all-campaigns-button',
+    content: '📈 Download all of your campaign data in one go!',
+  },
+  {
+    selector: '#campaign-1',
+    content:
+      '🔍️ Click a campaign to see its detail page. Click back to return here.',
+  },
+  {
+    selector: '#campaign-1-support-button',
+    content:
+      '❓️ Get personalized help quickly with a simple ready-to-go email.',
+  },
+  {
+    selector: '#campaign-1-payment-button',
+    content:
+      '💳️ Regenerate and email a payment form to yourself effortlessly.',
+  },
+  {
+    selector: '#campaign-0-repeat-button',
+    content: '♻️ Repeat campaigns with ease by pressing "repeat".',
+  },
+  {
+    selector: '#profile-form',
+    content: '👤 Update your information in the settings section.',
+  },
+];
