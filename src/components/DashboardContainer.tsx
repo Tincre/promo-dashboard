5536f536512a8fe3362d2af68ac34290f6b29dbc
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-03-26 17:16:35 -0500

    :label: add promo-dashboard-container id #19

--- src/components/DashboardContainer.tsx
+++ src/components/DashboardContainer.tsx
@@ -8,7 +8,10 @@ import { ReactNode } from 'react';
 
 export function DashboardContainer({ children }: { children?: ReactNode }) {
   return (
-    <div className="mx-auto max-w-7xl px-2 py-2 sm:px-6 sm:py-6 lg:px-8 lg:py-8">
+    <div
+      className="mx-auto max-w-7xl px-2 py-2 sm:px-6 sm:py-6 lg:px-8 lg:py-8"
+      id="promo-dashboard-container"
+    >
       {children}
     </div>
   );
