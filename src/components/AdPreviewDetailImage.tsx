7a29256695c375c29ab883e7ceb82a9028ae14e0
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-04-19 18:08:11 -0500

    :sparkles: add video rendering to campaign details #20

--- src/components/AdPreviewDetailImage.tsx
+++ src/components/AdPreviewDetailImage.tsx
@@ -4,16 +4,52 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+import { useEffect, useState } from 'react';
+const VIDEO_EXTENSIONS = [
+  'm3u8',
+  'ts',
+  'm2ts',
+  'mts',
+  'mov',
+  'mkv',
+  'mp4',
+  'mpeg',
+  'mpd',
+];
+const checkIsVideo = (creativeUrl: string) => {
+  const extension = creativeUrl
+    .split('/')
+    .slice(-1)[0]
+    .split('.')
+    .slice(-1)[0]
+    .toLowerCase();
+  return VIDEO_EXTENSIONS.includes(extension) ? true : false;
+};
 
 export function AdPreviewDetailImage({ creativeUrl }: { creativeUrl: string }) {
+  const [isVideo, setIsVideo] = useState<boolean | undefined>(undefined);
+  useEffect(() => {
+    if (typeof isVideo === 'undefined') {
+      setIsVideo(checkIsVideo(creativeUrl));
+    }
+  }, [isVideo]);
   return (
     <div className="relative ">
-      {' '}
-      <img
-        src={creativeUrl}
-        className="rounded-lg object-cover w-full h-56 sm:h-72 md:h-96"
-        alt=""
-      />
+      {!isVideo ? (
+        <img
+          src={creativeUrl}
+          className="rounded-lg object-cover w-full h-56 sm:h-72 md:h-96"
+          alt=""
+        />
+      ) : (
+        <video
+          muted
+          controls
+          className="rounded-lg object-cover w-full h-56 sm:h-72 md:h-96"
+        >
+          <source src={creativeUrl} type="video/mp4" />
+        </video>
+      )}
     </div>
   );
 }
