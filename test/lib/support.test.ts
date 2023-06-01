26c389ebb35415928d863bac81d5bddc60b427d1
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-06-01 08:27:50 -0500

    :white_check_mark: add test coverage for customized emailDomain and emailLocalPart parameters #15

--- test/lib/support.test.ts
+++ test/lib/support.test.ts
@@ -2,8 +2,18 @@ import { getSupportLink } from '../../src/lib/support';
 import { campaignStubData } from '../cms.data';
 
 describe('getSupportLink', () => {
-  it('returns the prepared email support link', () => {
+  it('returns the prepared email support link using defaults', () => {
     const supportLink = getSupportLink(campaignStubData[0]);
     expect(typeof supportLink).toBe('string');
+    expect(supportLink).toContain('team@tincre.dev');
+  });
+  it('returns the prepared email support link using parameters', () => {
+    const supportLink = getSupportLink(
+      campaignStubData[0],
+      'tincre.com',
+      'team'
+    );
+    expect(typeof supportLink).toBe('string');
+    expect(supportLink).toContain('team@tincre.com');
   });
 });
