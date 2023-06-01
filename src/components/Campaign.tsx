a1deb56bfffe8e98d94df3b75b7f3a1d2c4af7ae
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-06-01 08:16:52 -0500

    :recycle: refactor to use imported getSupportLink function from lib module #15

--- src/components/Campaign.tsx
+++ src/components/Campaign.tsx
@@ -11,22 +11,8 @@ import { CampaignData } from '../lib/types';
 import { CampaignRepeatButton } from './CampaignRepeatButton';
 import { CampaignSupportButton } from './CampaignSupportButton';
 import { CampaignPaymentButton } from './CampaignPaymentButton';
+import { getSupportLink } from '../lib/support';
 
-const supportDomain = 'tincre.dev';
-const getSupportLink = (data: any) =>
-  `mailto:team@${supportDomain}?subject=${
-    data.pid
-  }%20-%20Support%20request%20from%20${supportDomain}&body=Hi!%20I%20am%20reaching%20out%20to%20request%20support%20for%20campaign%20${
-    data.pid
-  }.%0A%0AMy%20issue%3A%0A%0A%0A%0ACampaign%20input%20data%3A%0A%20%20Ad%20Title%3A%20${
-    data?.adTitle || ''
-  }%0A%20%20Target%20link%3A%20${data?.target || ''}%0A%20%20Budget%3A%20${
-    data?.budget || ''
-  }%0A%20%20Ad%20Copy%3A%20${
-    data?.adCopy || ''
-  }%0A%20%20Call%20to%20action%3A%20${
-    data?.callToAction || ''
-  }%0A%20%20Button%20Text%3A%20${data?.buttonText || ''}`;
 const VIDEO_EXTENSIONS = [
   'm3u8',
   'ts',
@@ -144,7 +130,11 @@ export function Campaign({
               alt={adTitle}
             />
           ) : (
-            <video muted controls className="mx-auto h-32 w-full flex-shrink-0 rounded-b-md rounded-t-sm object-cover px-2">
+            <video
+              muted
+              controls
+              className="mx-auto h-32 w-full flex-shrink-0 rounded-b-md rounded-t-sm object-cover px-2"
+            >
               <source src={creativeUrl} type="video/mp4" />
             </video>
           )}
