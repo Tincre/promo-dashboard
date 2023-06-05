6517e034e1cb61ce9b3b4181120880acdbe4a540
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-06-04 20:13:46 -0500

    :sparkles: add component for delete button 'X' #23 #6

---
+++ src/components/CampaignDeleteButton.tsx
@@ -0,0 +1,46 @@
+/* Copyright Tincre (Musicfox, Inc)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+import { MouseEvent, ReactNode } from 'react';
+import { CampaignData } from '../lib/types';
+import { XCircleIcon } from '@heroicons/react/20/solid';
+
+export function CampaignDeleteButton({
+  data,
+  handleDeleteButtonOnClick,
+  id,
+}: {
+  data: CampaignData;
+  handleDeleteButtonOnClick?: (
+    event: MouseEvent<HTMLButtonElement>,
+    data: CampaignData
+  ) => void;
+  children?: ReactNode;
+  id?: string;
+}) {
+  return (
+    <button
+      aria-label={`promo-dashboard-campaign-delete-${
+        data?.pid || 'default'
+      }-button`}
+      type="button"
+      onClick={(event: MouseEvent<HTMLButtonElement>) => {
+        return typeof handleDeleteButtonOnClick !== 'undefined'
+          ? handleDeleteButtonOnClick(event, data)
+          : console.warn(
+              `promo-dashboard::CampaignDeleteButton::Undefined handleDeleteButtonOnClick. Please contact the developer of this application and report this error.`
+            );
+      }}
+      id={id}
+      className="absolute inset-x-0 bottom-8 sm:bottom-10 mx-auto z-10"
+    >
+      <XCircleIcon
+        className="h-8 w-8 sm:h-6 sm:w-6 text-red-500 hover:text-red-800 group-hover:text-red-500 group-hover:bg-slate-800 group-hover:rounded-full"
+        aria-hidden="true"
+      />
+    </button>
+  );
+}
