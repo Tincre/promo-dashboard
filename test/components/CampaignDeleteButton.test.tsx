ca781d080e15e4f3f7884a3bc2da3e334203d992
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-06-07 17:02:48 -0500

    :white_check_mark: add component specific ut for CampaignDeleteButton

---
+++ test/components/CampaignDeleteButton.test.tsx
@@ -0,0 +1,55 @@
+import React, { MouseEvent } from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { CampaignDeleteButton } from '../../src/components/CampaignDeleteButton';
+import { campaignStubData } from '../cms.data';
+import { CampaignData } from '../../src/lib/types';
+
+describe('CampaignDeleteButton', () => {
+  let testData = campaignStubData[0];
+  it('renders without crashing', () => {
+    testData.receiptId = undefined;
+    render(<CampaignDeleteButton data={testData} />);
+    const button = screen.getByLabelText(/campaign-delete/i);
+    fireEvent.click(button);
+    expect(button).toBeDefined();
+  });
+  it('renders with children without crashing', () => {
+    testData.receiptId = '';
+    render(
+      <CampaignDeleteButton
+        data={campaignStubData[0]}
+        id="test-id-delete-button"
+      />
+    );
+    const button = screen.getByLabelText(/campaign-delete/i);
+    fireEvent.click(button);
+    expect(button).toBeDefined();
+  });
+  it('renders with handleDeleteButtonOnClick without crashing', () => {
+    let testLocalData: CampaignData | undefined = undefined;
+    let testEvent: MouseEvent<HTMLButtonElement> | undefined = undefined;
+    let testFlag: boolean = false;
+    testData.receiptId = undefined;
+    const handleDeleteButtonOnClick = (
+      event: MouseEvent<HTMLButtonElement>,
+      data: CampaignData
+    ) => {
+      testFlag = true;
+      testLocalData = data;
+      testEvent = event;
+    };
+    render(
+      <CampaignDeleteButton
+        data={campaignStubData[0]}
+        handleDeleteButtonOnClick={handleDeleteButtonOnClick}
+        id="test-id-delete-button"
+      />
+    );
+    const button = screen.getByLabelText(/campaign-delete/i);
+    fireEvent.click(button);
+    expect(button).toBeDefined();
+    expect(testFlag).toBeTruthy();
+    expect(testEvent).toBeDefined();
+    expect(testLocalData).toMatchObject(testData);
+  });
+});
