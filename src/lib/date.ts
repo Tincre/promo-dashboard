561e920e16cefeb04055406520b35285581d12b9
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-08-31 18:24:14 -0500

    :sparkles: add date utility functions

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -8,3 +8,45 @@ export function now() {
   const event = new Date();
   return event.toISOString().slice(0, 10); // don't include time
 }
+
+export function generateDateRange(
+  startDate: Date,
+  endDate?: Date,
+  includeEndDate?: boolean
+) {
+  const dates = [];
+  const currentDate = startDate;
+  if (typeof endDate === 'undefined') endDate = new Date();
+  while (currentDate < endDate) {
+    dates.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+  if (includeEndDate) dates.push(endDate);
+  return dates;
+}
+
+export function generateStringDateRanges(
+  startDate: string,
+  endDate?: string,
+  includeEndDate?: boolean
+) {
+  const tmpDates: Date[] =
+    typeof endDate !== 'undefined'
+      ? generateDateRange(
+          new Date(startDate),
+          new Date(endDate),
+          includeEndDate
+        )
+      : generateDateRange(new Date(startDate), undefined, includeEndDate);
+  return tmpDates.map((date) => date.toISOString().slice(0, 10));
+}
+export function generateDates(length: number, endDate?: Date) {
+  const dates: string[] = [];
+  const currentDate = endDate || new Date();
+  for (let i: number = length; i > 0; i--) {
+    let tmpDate = new Date(currentDate);
+    dates.push(tmpDate.toISOString().slice(0, 10));
+    currentDate.setDate(currentDate.getDate() - 1);
+  }
+  return dates.reverse();
+}
