58540ba3a7a17f2a1c0109df2a7ea2960ea23c6e
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-08-31 18:03:15 -0500

    :sparkles: add test for generateDates func

--- test/lib/date.test.ts
+++ test/lib/date.test.ts
@@ -1,9 +1,32 @@
-import { now } from '../../src/lib/date';
+import {
+  now,
+  generateDates,
+  generateDateRange,
+  generateStringDateRanges,
+} from '../../src/lib/date';
 
 describe('now', () => {
   it('returns the date timestamp right now', () => {
     const time = now();
     expect(typeof time).toBe('string');
     expect(time.length).toBe(10);
+  });
+});
+
+describe('generateDates', () => {
+  it('returns an array of ISO date strings', () => {
+    const testLength = 10;
+    const testDates = generateDates(testLength);
+    expect(Array.isArray(testDates)).toBe(true);
+    expect(testDates.length).toBe(testLength);
+    const date = now();
+    expect(testDates[testLength - 1]).toBe(date);
+  });
+  it('returns an array of ISO date strings w/custom end date', () => {
+    const testLength = 10;
+    const testDates = generateDates(testLength, new Date('2023-01-03'));
+    expect(Array.isArray(testDates)).toBe(true);
+    expect(testDates.length).toBe(testLength);
+    expect(testDates[testLength - 1]).toBe('2023-01-03');
   });
 });
