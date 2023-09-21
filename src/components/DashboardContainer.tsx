7b15043b09fda4b06385a7b99aff9042e5f240ee
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-09-21 11:49:47 -0600

    :recycle: refactor to add animate-pulse on isLoading truthy prop #37

--- src/components/DashboardContainer.tsx
+++ src/components/DashboardContainer.tsx
@@ -6,12 +6,18 @@
  */
 import { ReactNode } from 'react';
 
-export function DashboardContainer({ children }: { children?: ReactNode }) {
+export function DashboardContainer({
+  children,
+  isLoading,
+}: {
+  children?: ReactNode;
+  isLoading?: boolean;
+}) {
+  const className = isLoading
+    ? 'animate-pulse mx-auto max-w-7xl px-2 py-2 sm:px-6 sm:py-6 lg:px-8 lg:py-8'
+    : 'mx-auto max-w-7xl px-2 py-2 sm:px-6 sm:py-6 lg:px-8 lg:py-8';
   return (
-    <div
-      className="mx-auto max-w-7xl px-2 py-2 sm:px-6 sm:py-6 lg:px-8 lg:py-8"
-      id="promo-dashboard-container"
-    >
+    <div className={className} id="promo-dashboard-container">
       {children}
     </div>
   );
