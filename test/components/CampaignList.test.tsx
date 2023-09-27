ee4ac0b1d8827276fbe003c5318f1c474d553ccc
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-09-27 13:48:00 -0600

    :recycle: :wrench: add intersection observer mocking

--- test/components/CampaignList.test.tsx
+++ test/components/CampaignList.test.tsx
@@ -2,9 +2,27 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { CampaignList } from '../../src/components/CampaignList';
 import { campaignStubData } from '../cms.data';
+import {
+  mockAllIsIntersecting,
+  setupIntersectionMocking,
+  resetIntersectionMocking,
+} from 'react-intersection-observer/test-utils';
+
+window.scrollTo = jest.fn();
+
+beforeEach(() => {
+  setupIntersectionMocking(jest.fn);
+});
+afterEach(() => {
+  resetIntersectionMocking();
+});
+afterAll(() => {
+  jest.clearAllMocks();
+});
 
 describe('CampaignList', () => {
   it('renders full data without crashing', () => {
+    mockAllIsIntersecting(true); 
     render(
       <CampaignList
         data={campaignStubData}
