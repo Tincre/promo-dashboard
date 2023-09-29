3ce1df234085bf3bb1458ee47fcf00c43ca42880
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-09-29 11:53:16 -0600

    :sparkles: add statsHighlightTimeseries data disection by date

--- src/components/CampaignsSummaryStats.tsx
+++ src/components/CampaignsSummaryStats.tsx
@@ -4,10 +4,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, MouseEvent, useState, useEffect } from 'react';
 import { CampaignStatsData } from '../lib/types';
 import { CampaignsStatsHighlights } from './CampaignsStatsHighlights';
 import { CampaignsChart } from './CampaignsChart';
+import { merge } from '../lib/merge';
+import { options } from '../lib/options';
+import { disectChartData } from '../lib/disectChartData';
 
 export function CampaignsSummaryStats({
   data,
@@ -19,13 +22,58 @@ export function CampaignsSummaryStats({
   statsHighlightTimeseries?: CampaignStatsData;
   statsHighlightMetricName?: string;
   handleCampaignDetailBackOnClick: MouseEventHandler<HTMLButtonElement>;
-  handleStatsHighlightClick?: Function;
+  handleStatsHighlightClick?: Function /* TODO type this */;
 }) {
+  const [selectedChartButton, setSelectedChartButton] = useState(
+    options.timePeriods[0]
+  );
+  const [
+    internalStatsHighlightTimeseries,
+    setInternalStatsHighlightTimeseries,
+  ] = useState<CampaignStatsData | undefined>();
+  useEffect(() => {
+    if (typeof statsHighlightTimeseries !== 'undefined') {
+      setInternalStatsHighlightTimeseries(
+        merge(statsHighlightTimeseries, {
+          chartData: disectChartData(
+            statsHighlightTimeseries?.chartData?.labels as (string | null)[],
+            statsHighlightTimeseries?.chartData?.data as (
+              | string
+              | null
+              | number
+            )[],
+            options.timePeriodDayLengths.get(selectedChartButton) || 30
+          ),
+        }) as CampaignStatsData
+      );
+    }
+  }, [statsHighlightTimeseries]);
+  const handleChartButtonOnClick = async (
+    event: MouseEvent<HTMLButtonElement>,
+    buttonType: string
+  ) => {
+    event.preventDefault();
+    setSelectedChartButton(buttonType);
+    setInternalStatsHighlightTimeseries(
+      merge(internalStatsHighlightTimeseries || {}, {
+        chartData: disectChartData(
+          statsHighlightTimeseries?.chartData?.labels || [],
+          statsHighlightTimeseries?.chartData?.data || [],
+          options.timePeriodDayLengths.get(buttonType) || 30
+        ),
+      }) as CampaignStatsData
+    );
+  };
   return (
     <>
       {!data ? null : (
         <>
-          <CampaignsChart statsHighlightTimeseries={statsHighlightTimeseries} />
+          <CampaignsChart
+            timePeriods={options.timePeriods}
+            statsHighlightTimeseries={internalStatsHighlightTimeseries}
+            selectedChartButton={selectedChartButton}
+            handleChartButtonOnClick={handleChartButtonOnClick}
+          />
           <CampaignsStatsHighlights
             stats={data}
             handleStatsHighlightClick={handleStatsHighlightClick}
