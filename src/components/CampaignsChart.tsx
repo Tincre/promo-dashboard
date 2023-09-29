da8a84f7b31a6d9ec0f1fa57938d7dee1ba1b584
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-09-29 11:52:43 -0600

    :sparkles: add time buttons + selections

--- src/components/CampaignsChart.tsx
+++ src/components/CampaignsChart.tsx
@@ -4,7 +4,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+import { MouseEvent, useState } from 'react';
 import { CampaignsLineChart } from './CampaignsLineChart';
+import { ChartButton } from './ChartButton';
 import {
   CampaignData,
   CampaignDummyData,
@@ -12,17 +14,38 @@ import {
 } from '../lib/types';
 
 export function CampaignsChart({
+  timePeriods,
   statsHighlightTimeseries,
+  selectedChartButton,
+  handleChartButtonOnClick,
 }: {
+  timePeriods: Array<string>;
   statsHighlightTimeseries?: CampaignStatsData;
+  selectedChartButton: string;
+  handleChartButtonOnClick: (
+    event: MouseEvent<HTMLButtonElement>,
+    timePeriod: string
+  ) => void;
 }) {
   return (
-    <div className="mt-5 grid grid-cols-1 gap-5">
-      <div className="relative overflow-hidden rounded-lg bg-slate-50 px-4 py-5 pb-0 shadow sm:px-6 sm:py-6 dark:bg-slate-700">
-        {!statsHighlightTimeseries ? null : (
-          <CampaignsLineChart info={statsHighlightTimeseries} />
-        )}
+    <>
+      <div className="inline-flex flex-nowrap w-full justify-center sm:justify-items-start">
+        {timePeriods.map((timePeriod, index) => (
+          <ChartButton
+            isSelected={selectedChartButton === timePeriod}
+            onClick={handleChartButtonOnClick}
+          >
+            {timePeriod}
+          </ChartButton>
+        ))}
       </div>
-    </div>
+      <div className="mt-5 grid grid-cols-1 gap-5">
+        <div className="relative overflow-hidden rounded-lg bg-slate-50 px-4 py-5 pb-0 shadow sm:px-6 sm:py-6 dark:bg-slate-700">
+          {!statsHighlightTimeseries ? null : (
+            <CampaignsLineChart info={statsHighlightTimeseries} />
+          )}
+        </div>
+      </div>
+    </>
   );
 }
