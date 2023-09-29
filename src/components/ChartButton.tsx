7765d0985f59b9abc951d440624198652e1e45a2
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-09-29 12:34:57 -0600

    :recycle: :art: update xy padding for buttons

--- src/components/ChartButton.tsx
+++ src/components/ChartButton.tsx
@@ -6,9 +6,9 @@
  */
 import { MouseEvent, useState } from 'react';
 const isHighlightedClassName =
-  'relative overflow-hidden rounded-lg bg-slate-200 px-1 py-2 text-xs md:text-sm shadow sm:px-2 sm:py-3 hover:bg-slate-200 hover:shadow-lg border border-1 border-slate-700 dark:border-slate-300 dark:bg-slate-600 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:border-slate-200 mx-1';
+  'relative overflow-hidden rounded-lg bg-slate-200 px-2 py-1 text-xs md:text-sm shadow sm:px-3 sm:py-2 hover:bg-slate-200 hover:shadow-lg border border-1 border-slate-700 dark:border-slate-300 dark:bg-slate-600 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:border-slate-200 mx-2 sm:mx-3';
 const isNotHighlightedClassName =
-  'relative overflow-hidden rounded-lg bg-slate-50 px-1 py-2 text-xs md:text-sm shadow sm:px-2 sm:py-3 hover:bg-slate-200 hover:shadow-lg border border-1 border-transparent dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 mx-1';
+  'relative overflow-hidden rounded-lg bg-slate-50 px-2 py-1 text-xs md:text-sm shadow sm:px-3 sm:py-2 hover:bg-slate-200 hover:shadow-lg border border-1 border-transparent dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 mx-2 sm:mx-3';
 
 export function ChartButton({
   isSelected = false,
