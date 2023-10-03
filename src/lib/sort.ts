210f136b943a7ec5d7b6c55f9d25dd6bbbd65373
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-10-03 10:41:45 -0600

    :sparkles: add function to sort by active, paid, and date

--- src/lib/sort.ts
+++ src/lib/sort.ts
@@ -5,7 +5,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 import { CampaignData, CampaignDummyData } from './types';
-
+import { compareDates } from './compareDates';
 export function sortCampaignDataOnIsActive(
   data: CampaignData[] | CampaignDummyData[]
 ) {
@@ -28,6 +28,42 @@ export function sortCampaignDataOnIsActiveAndReceiptId(
   );
   return newArray;
 }
+
+export function sortCampaignDataOnIsActiveAndReceiptIdByDate(
+  data: CampaignData[] | CampaignDummyData[],
+  isAscending?: boolean
+) {
+  let runningCampaigns: CampaignData[] = [];
+  let completedCampaigns: CampaignData[] = [];
+  let inactiveCampaigns: CampaignData[] = [];
+  let unpaidCampaigns: CampaignData[] = [];
+  data.forEach((campaign: CampaignData | CampaignDummyData) => {
+    if (campaign?.isActive && campaign?.receiptId) {
+      // @ts-ignore
+      runningCampaigns.push(campaign);
+      return;
+    }
+    if (campaign?.isActive && !campaign?.receiptId) {
+      // @ts-ignore
+      unpaidCampaigns.push(campaign);
+      return;
+    }
+    if (!campaign?.isActive && campaign?.receiptId) {
+      // @ts-ignore
+      completedCampaigns.push(campaign);
+      return;
+    } // @ts-ignore
+    inactiveCampaigns.push(campaign);
+  });
+
+  return [
+    ...runningCampaigns.sort((a, b) => compareDates(a, b, isAscending)),
+    ...completedCampaigns.sort((a, b) => compareDates(a, b, isAscending)),
+    ...unpaidCampaigns.sort((a, b) => compareDates(a, b, isAscending)),
+    ...inactiveCampaigns.sort((a, b) => compareDates(a, b, isAscending)),
+  ];
+}
+
 export const numActiveCampaigns = (
   sortedCampaignsData: CampaignData[] | CampaignDummyData[],
   exclusions: string[] = []
