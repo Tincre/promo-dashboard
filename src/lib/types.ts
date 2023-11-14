4d715f5799995adc99fe8577b3131fe34cdbefbc
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-14 16:48:32 -0500

    :label: add isActive to CampaignSortedData

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -78,7 +78,7 @@ export interface CampaignStatsData {
   id: number | undefined;
   name: CampaignMetrics;
   stat: string;
-
+  isActive?: boolean;
   icon: React.ForwardRefExoticComponent<
     Omit<React.SVGProps<SVGSVGElement>, 'ref'> & {
       title?: string | undefined;
