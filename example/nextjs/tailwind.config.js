6cdd8c00c3714c577b3798fa8441fc5e4682291f
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-26 20:58:17 -0500

    :bug: fix tailwind purge dirs + add typography utility

--- example/nextjs/tailwind.config.js
+++ example/nextjs/tailwind.config.js
@@ -3,14 +3,12 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     // uncomment for npm @tincre/promo-dashboard installation
-    './node_modules/@tincre/promo-dashboard/*.{js,ts,jsx,tsx}',
-    './node_modules/@tincre/promo-dashboard/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@tincre/promo-button/*',
-    './node_modules/@tincre/promo-button/**/*',
-    '../../dist/*.{js,ts,jsx,tsx}', // remove for deployed apps
+    './node_modules/@tincre/promo-dashboard/**',
+    './node_modules/@tincre/promo-button/**',
+    './node_modules/@tincre/promo-chat/**',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: ['@tailwindcss/typography'],
 };
