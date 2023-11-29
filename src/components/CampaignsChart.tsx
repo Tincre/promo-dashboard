3a677175df17e144460b6311db9e3f1220f4744f
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-28 23:49:50 -0500

    :fire: remove useState unused import

--- src/components/CampaignsChart.tsx
+++ src/components/CampaignsChart.tsx
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-import { MouseEvent, useState } from 'react';
+import { MouseEvent } from 'react';
 import { CampaignsLineChart } from './CampaignsLineChart';
 import { ChartButton } from './ChartButton';
 import { CampaignStatsData } from '../lib/types';
