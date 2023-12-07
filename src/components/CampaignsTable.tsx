6c1a120f28d7dc525fe41f213f811886111996ae
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-12-07 16:58:07 -0600

    :art: add dark mode for the dashboard table

--- src/components/CampaignsTable.tsx
+++ src/components/CampaignsTable.tsx
@@ -39,47 +39,51 @@ export function CampaignsTable({
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <div className="group rounded-md">
+        <div
+          className="group rounded-md"
+          id="promo-dashboard-campaigns-table-container"
+        >
           <div
+            id="promo-dashboard-campaigns-table-main"
             className={
-              'h-[380px] overflow-y-auto overflow-x-hidden border border-gray-50 bg-gray-50 shadow-lg rounded-md group-hover:bg-gray-100 group-hover:shadow-lg'
+              'h-[380px] overflow-y-auto overflow-x-hidden border border-gray-50 bg-gray-50 shadow-lg rounded-md group-hover:bg-gray-100 dark:group-hover:bg-slate-900 group-hover:shadow-lg dark:bg-slate-800 dark:border-slate-800'
             }
           >
             <div className={'px-4 sm:px-6 lg:px-8 block'}>
               <div className="-mx-4 mt-8 sm:-mx-0">
                 <table className="min-w-full divide-y divide-gray-300">
                   <thead className="">
-                    <tr className="">
+                    <tr className="dark:bg-slate-800">
                       <th
                         scope="col"
-                        className="py-3.5 pl-4 pr-3 pb-2 text-left text-sm font-semibold text-gray-900 sm:pl-0 sticky top-0 bg-gray-50 group-hover:bg-gray-100 rounded-md select-none"
+                        className="py-3.5 pl-4 pr-3 pb-2 text-left text-sm font-semibold text-gray-900 sm:pl-0 sticky top-0 bg-gray-50 group-hover:bg-gray-100 select-none dark:bg-slate-800 dark:border-slate-700 dark:group-hover:bg-slate-900 dark:text-slate-100"
                       >
                         Ads
                       </th>
                       <th
                         scope="col"
-                        className="hidden px-3 py-3.5 pb-2 text-left text-sm font-semibold text-gray-900 md:table-cell sticky top-0 bg-gray-50 group-hover:bg-gray-100 rounded-md select-none"
+                        className="hidden px-3 py-3.5 pb-2 text-left text-sm font-semibold text-gray-900 md:table-cell sticky top-0 bg-gray-50 group-hover:bg-gray-100 select-none dark:bg-slate-800 dark:border-slate-700 dark:group-hover:bg-slate-900 dark:text-slate-100"
                       >
                         Campaign ID
                       </th>
                       <th
                         scope="col"
-                        className="hidden px-3 py-3.5 pb-2 text-left text-sm font-semibold text-gray-900 sm:table-cell sticky top-0 bg-gray-50 group-hover:bg-gray-100 rounded-md select-none"
+                        className="hidden px-3 py-3.5 pb-2 text-left text-sm font-semibold text-gray-900 sm:table-cell sticky top-0 bg-gray-50 group-hover:bg-gray-100 select-none dark:bg-slate-800 dark:border-slate-700 dark:group-hover:bg-slate-900 dark:text-slate-100"
                       >
                         Budget
                       </th>
                       <th
                         scope="col"
-                        className="px-3 py-3.5 pb-2 text-left text-sm font-semibold text-gray-900 sticky top-0 bg-gray-50 group-hover:bg-gray-100 rounded-md select-none"
+                        className="px-3 py-3.5 pb-2 text-left text-sm font-semibold text-gray-900 sticky top-0 bg-gray-50 group-hover:bg-gray-100 select-none dark:bg-slate-800 dark:border-slate-700 dark:group-hover:bg-slate-900 dark:text-slate-100"
                       >
                         Status
                       </th>
                     </tr>
                   </thead>
-                  <tbody className="divide-y divide-gray-200 bg-gray-50 group-hover:bg-gray-100 rounded-md">
+                  <tbody className="divide-y divide-gray-200 bg-gray-50 group-hover:bg-gray-100 dark:bg-slate-800 dark:border-slate-700 dark:group-hover:bg-slate-900 dark:text-slate-100">
                     {data.map((campaign, index) => (
                       <tr
-                        className="cursor-pointer hover:text-gray-200"
+                        className="cursor-pointer hover:text-gray-200 dark:bg-slate-800 dark:group-hover:bg-slate-900"
                         key={`${campaign.adTitle}-${index}`}
                         onClick={(e) => {
                           e.preventDefault();
@@ -87,23 +91,23 @@ export function CampaignsTable({
                           handleCampaignClick(e, campaign);
                         }}
                       >
-                        <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-normal text-gray-800 sm:w-auto sm:max-w-none sm:pl-0 text-left">
+                        <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-normal text-gray-800 sm:w-auto sm:max-w-none sm:pl-0 text-left dark:bg-slate-800 dark:border-slate-700 dark:group-hover:bg-slate-900 dark:text-slate-100">
                           {campaign.adTitle}
                           <dl className="font-normal md:hidden">
                             <dt className="sr-only">Campaign ID</dt>
-                            <dd className="mt-1 truncate text-gray-700 text-left md:hidden">
+                            <dd className="mt-1 truncate text-gray-700 text-left md:hidden dark:text-slate-100">
                               {campaign.pid}
                             </dd>
                             <dt className="sr-only sm:hidden">Budget</dt>
-                            <dd className="mt-1 truncate text-gray-500 sm:hidden">
+                            <dd className="mt-1 truncate text-gray-500 sm:hidden dark:text-slate-200">
                               {campaign.budget}
                             </dd>
                           </dl>
                         </td>
-                        <td className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell text-left">
+                        <td className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell text-left dark:text-slate-200">
                           {campaign.pid}
                         </td>
-                        <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell text-left">
+                        <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell text-left dark:text-slate-200">
                           {campaign.budget}
                         </td>
                         <td className="px-3 py-4 text-sm text-gray-500 text-left">
