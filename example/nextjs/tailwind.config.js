2713749b8c3ab05af71d9719518673c2f52e5e53
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-12-07 18:06:12 -0600

    :recycle: update purge path to include local directory comment

--- example/nextjs/tailwind.config.js
+++ example/nextjs/tailwind.config.js
@@ -4,11 +4,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     // uncomment for npm @tincre/promo-dashboard installation
     './node_modules/@tincre/promo-dashboard/**',
+    //    '../../dist/**',
     './node_modules/@tincre/promo-button/**',
     './node_modules/@tincre/promo-chat/**',
   ],
   theme: {
-    extend: {},
+    extend: {
+      textOpacity: ['dark'],
+    },
   },
   plugins: ['@tailwindcss/typography'],
 };
