c3598400fbe6c4919bf87657fac7c016f4a626b9
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-12-27 17:19:58 -0600

    :white_check_mark: add complete full coverage

--- test/lib/notifications.test.ts
+++ test/lib/notifications.test.ts
@@ -2,6 +2,7 @@ import {
   successToast,
   failureToast,
   infoToast,
+  copyToast,
 } from '../../src/lib/notifications';
 
 describe('successToast', () => {
@@ -9,6 +10,10 @@ describe('successToast', () => {
     const toast = successToast('Success!');
     expect(toast).toBeTruthy();
     expect(typeof toast).toBe('string');
+  });
+  it('returns the custom success toast', () => {
+    const toast = successToast('Success!', 'blah');
+    expect(toast).toBeTruthy();
   });
 });
 
@@ -18,11 +23,33 @@ describe('failureToast', () => {
     expect(toast).toBeTruthy();
     expect(typeof toast).toBe('string');
   });
+  it('returns the custom failure toast', () => {
+    const toast = failureToast('Failure!', 'blah');
+    expect(toast).toBeTruthy();
+    expect(typeof toast).toBe('string');
+  });
 });
 
 describe('infoToast', () => {
   it('returns the info toast', () => {
     const toast = infoToast('Info!');
+    expect(toast).toBeTruthy();
+    expect(typeof toast).toBe('string');
+  });
+  it('returns the custom info toast', () => {
+    const toast = infoToast('Info!', 'blah');
+    expect(toast).toBeTruthy();
+    expect(typeof toast).toBe('string');
+  });
+});
+describe('copyToast', () => {
+  it('returns the default copy toast', () => {
+    const toast = copyToast('Info!');
+    expect(toast).toBeTruthy();
+    expect(typeof toast).toBe('string');
+  });
+  it('returns the custom copy toast', () => {
+    const toast = copyToast('Info!', 'blah');
     expect(toast).toBeTruthy();
     expect(typeof toast).toBe('string');
   });
