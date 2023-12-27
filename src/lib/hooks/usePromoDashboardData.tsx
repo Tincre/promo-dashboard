e4f895d25ed709d50e651a6c7d302b25571cb4a1
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-12-27 14:15:59 -0600

    :sparkles: add combined usePromoDashboardData hook

---
+++ src/lib/hooks/usePromoDashboardData.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import {
+  CampaignData,
+  CampaignDummyData,
+  CampaignSortedData,
+} from '@tincre/promo-types';
+import { sortCampaignDataOnIsActiveAndReceiptIdByDate } from '../sort';
+import { replaceDataParamForChartData } from '../coerce';
+import { numActiveCampaigns } from '../sort';
+
+/**
+ * usePromoDashboardData
+ * @description A React hook that returns the sorted dashboard data and number of active campaigns.
+ */
+export function usePromoDashboardData(
+  campaignsData: CampaignData[] | CampaignDummyData[],
+  deletedCampaigns: string[]
+) {
+  const [numberOfActiveCampaigns, setNumberOfActiveCampaigns] = useState<
+    number | undefined
+  >();
+  const [sortedCampaignsData, setSortedCampaignsData] = useState<
+    CampaignSortedData[]
+  >([]);
+  useEffect(() => {
+    if (typeof campaignsData !== 'undefined') {
+      setSortedCampaignsData(
+        replaceDataParamForChartData(
+          sortCampaignDataOnIsActiveAndReceiptIdByDate(campaignsData)
+        )
+      );
+    } else setSortedCampaignsData([]);
+  }, [campaignsData]);
+  useEffect(() => {
+    if (sortedCampaignsData?.length) {
+      setNumberOfActiveCampaigns(
+        numActiveCampaigns(sortedCampaignsData, deletedCampaigns)
+      );
+    }
+  }, [sortedCampaignsData, deletedCampaigns]);
+  return { sortedCampaignsData, numberOfActiveCampaigns };
+}
---
+++ test/lib/hooks/usePromoDashboardData.test.tsx
@@ -0,0 +1,67 @@
+import { renderHook, act } from '@testing-library/react';
+import { usePromoDashboardData } from '../../../src/lib/hooks/usePromoDashboardData';
+import { sortCampaignDataOnIsActiveAndReceiptIdByDate } from '../../../src/lib/sort';
+import { replaceDataParamForChartData } from '../../../src/lib/coerce';
+import { numActiveCampaigns } from '../../../src/lib/sort';
+
+jest.mock('../../../src/lib/sort', () => ({
+  sortCampaignDataOnIsActiveAndReceiptIdByDate: jest.fn(),
+  numActiveCampaigns: jest.fn(),
+}));
+
+jest.mock('../../../src/lib/coerce', () => ({
+  replaceDataParamForChartData: jest.fn(),
+}));
+
+describe('usePromoDashboardData', () => {
+  it('returns initial state correctly', () => {
+    const { result } = renderHook(() => {
+      return usePromoDashboardData([], []);
+    });
+    expect(result.current.numberOfActiveCampaigns).toBeUndefined();
+  });
+
+  it('processes campaigns data and updates state accordingly', () => {
+    const mockCampaignsData = [
+      {
+        /* ... mock CampaignData or CampaignDummyData ... */
+      },
+    ];
+    const sortedMockData = [
+      {
+        /* ... sorted mock data ... */
+      },
+    ];
+    const transformedMockData = [
+      {
+        /* ... transformed mock data ... */
+      },
+    ];
+
+    // Mock the implementation
+    (sortCampaignDataOnIsActiveAndReceiptIdByDate as jest.Mock).mockReturnValue(
+      sortedMockData
+    );
+    (replaceDataParamForChartData as jest.Mock).mockReturnValue(
+      transformedMockData
+    );
+    (numActiveCampaigns as jest.Mock).mockReturnValue(5);
+
+    const { result, rerender } = renderHook(
+      ({ data, deleted }) => usePromoDashboardData(data, deleted),
+      {
+        initialProps: { data: mockCampaignsData, deleted: [] },
+      }
+    );
+
+    // Update hook props
+    rerender({ data: mockCampaignsData, deleted: [] });
+
+    expect(result.current.sortedCampaignsData).toEqual(transformedMockData);
+    expect(result.current.numberOfActiveCampaigns).toBe(5);
+  });
+
+  // Additional tests can be written for different scenarios, such as:
+  // - updating only `campaignsData` and checking the effect on `sortedCampaignsData`
+  // - updating only `deletedCampaigns` and observing the effect on `numberOfActiveCampaigns`
+});
