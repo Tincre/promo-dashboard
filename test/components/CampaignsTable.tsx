3991a29a3bc89a9c15107a3aca2fa7b4bc803d1b
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-12-27 17:32:31 -0600

    :white_check_mark: add explicit table tests

--- test/components/CampaignsTable.tsx
+++ test/components/CampaignsTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { CampaignsChart } from '../../src/components/CampaignsChart';
+import { CampaignsTable } from '../../src/components/CampaignsTable';
 import { campaignStubData } from '../cms.data';
 import { options } from '../../src/lib/options';
 import {
@@ -22,6 +23,8 @@ afterAll(() => {
 });
 
 describe('CampaignsChart', () => {
+  const handleCampaignClick = jest.fn();
+
   it('renders full data without crashing', () => {
     mockAllIsIntersecting(true);
     render(
@@ -35,5 +38,51 @@ describe('CampaignsChart', () => {
     const campaignsTableItem = screen.getByText(campaignStubData[0].pid);
     expect(campaignsTableItem).toBeDefined();
     fireEvent.click(campaignsTableItem);
+  });
+
+  it('renders the table with data and handles toggle', () => {
+    render(
+      <CampaignsTable
+        data={campaignStubData}
+        handleCampaignClick={handleCampaignClick}
+      />
+    );
+
+    // Check for table visibility
+    expect(screen.getByText('Collapse table')).toBeDefined();
+    expect(screen.getByText('Ad 1')).toBeDefined();
+    expect(screen.getByText('Ad 2')).toBeDefined();
+
+    // Simulate toggle
+    fireEvent.click(screen.getByText('Collapse table'));
+    expect(screen.getByText('Show table')).toBeDefined();
+
+    fireEvent.click(screen.getByText('Show table'));
+    expect(screen.getByText('Collapse table')).toBeDefined();
+    fireEvent.click(screen.getByText(`${campaignStubData[0].adTitle}-${0}`));
+  });
+
+  it('handles row click event', () => {
+    render(
+      <CampaignsTable
+        data={campaignStubData}
+        handleCampaignClick={handleCampaignClick}
+      />
+    );
+
+    // Simulate clicking on a table row
+    fireEvent.click(screen.getByText('Ad 1'));
+    expect(handleCampaignClick).toHaveBeenCalled();
+  });
+
+  it('renders correctly when isCollapsed is true', () => {
+    render(
+      <CampaignsTable
+        data={campaignStubData}
+        isCollapsed={true}
+        handleCampaignClick={handleCampaignClick}
+      />
+    );
+    expect(screen.getByText('Show table')).toBeDefined();
   });
 });
