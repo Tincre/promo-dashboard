f7c83af1e476b2538b388b1c3c568ca00ed9d070
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-12-27 22:27:47 -0600

    :recycle: refactor to use new useCreative hook

--- src/components/AdPreviewDetailImage.tsx
+++ src/components/AdPreviewDetailImage.tsx
@@ -4,42 +4,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-import { useEffect, useState } from 'react';
-const VIDEO_EXTENSIONS = [
-  'm3u8',
-  'ts',
-  'm2ts',
-  'mts',
-  'mov',
-  'mkv',
-  'mp4',
-  'mpeg',
-  'mpd',
-];
-const checkIsVideo = (creativeUrl: string) => {
-  const extension = creativeUrl
-    .split('/')
-    .slice(-1)[0]
-    .split('.')
-    .slice(-1)[0]
-    .toLowerCase();
-  return VIDEO_EXTENSIONS.includes(extension) ? true : false;
-};
+import { useCreative } from '../lib/hooks/useCreative';
 
-const getVideoPoster = (creativeUrl: string) =>
-  `${creativeUrl.substring(0, creativeUrl.lastIndexOf('.'))}.webp`;
 export function AdPreviewDetailImage({ creativeUrl }: { creativeUrl: string }) {
-  const [isVideo, setIsVideo] = useState<boolean | undefined>(undefined);
-  useEffect(() => {
-    if (typeof isVideo === 'undefined') {
-      setIsVideo(checkIsVideo(creativeUrl));
-    }
-  }, [isVideo]);
+  const { internalCreativeUrl, isVideo, videoPosterUrl } =
+    useCreative(creativeUrl);
   return (
     <div className="relative ">
       {!isVideo ? (
         <img
-          src={creativeUrl}
+          src={internalCreativeUrl}
           className="rounded-lg object-cover w-full h-56 sm:h-72 md:h-96"
           alt=""
         />
@@ -48,9 +22,9 @@ export function AdPreviewDetailImage({ creativeUrl }: { creativeUrl: string }) {
           muted
           controls
           className="rounded-lg object-cover w-full h-56 sm:h-72 md:h-96"
-          poster={getVideoPoster(creativeUrl)}
+          poster={videoPosterUrl}
         >
-          <source src={creativeUrl} type="video/mp4" />
+          <source src={internalCreativeUrl} type="video/mp4" />
         </video>
       )}
     </div>
