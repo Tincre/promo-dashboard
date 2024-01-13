91ebca56f6c41ccaaf419e28d23286725f7626ec
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-01-13 08:32:07 -0600

    :sparkles: :wrench: add internal default options for campaignTypes

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -13,4 +13,54 @@ export const options = {
     ['6 months', 180],
     ['12 months', 365],
   ]),
+  campaignTypes: [
+    {
+      name: 'Lead Generation',
+      description: 'Attract and acquire potential customers',
+      icon: 'magnifying-glass',
+      color: 'bg-violet-500',
+    },
+    {
+      name: 'Product Launch',
+      description: 'Introduce new products to market',
+      icon: 'rocket-launch',
+      color: 'bg-orange-500',
+    },
+    {
+      name: 'Event Promotion',
+      description: 'Advertise events to increase attendance',
+      icon: 'calendar',
+      color: 'bg-zinc-500',
+    },
+    {
+      name: 'Content Marketing',
+      description: 'Engage audience with valuable content',
+      icon: 'newspaper',
+      color: 'bg-teal-500',
+    },
+    {
+      name: 'Retargeting Ads',
+      description: 'Re-engage visitors with targeted ads',
+      icon: 'arrow-uturn-left',
+      color: 'bg-yellow-400',
+    },
+    {
+      name: 'Video Engagement',
+      description: 'Create engaging video content ads',
+      icon: 'video-camera',
+      color: 'bg-green-500',
+    },
+    {
+      name: 'Flash Sales',
+      description: 'Promote limited-time discount offers',
+      icon: 'tag',
+      color: 'bg-red-600',
+    },
+    {
+      name: 'Brand Awareness',
+      description: 'Boost brand recognition and visibility',
+      icon: 'eye',
+      color: 'bg-blue-500',
+    },
+  ],
 };
