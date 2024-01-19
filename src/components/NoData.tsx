2f87c66d1e7705a53df1499bab3081a90576c26e
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-01-18 21:10:52 -0600

    :art: update to include dark mode styling for ad types https://gitlab.com/tincre/tincre.com-v2/-/issues/7

--- src/components/NoData.tsx
+++ src/components/NoData.tsx
@@ -80,12 +80,12 @@ export function CampaignType({
     : `flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-lg bg-indigo-500`;
   const Icon = iconMap.get(icon || 'megaphone') || MegaphoneIcon;
   return (
-    <div className="relative -m-2 flex items-center space-x-4 rounded-xl p-2 focus-within:ring-2 focus-within:ring-indigo-500 hover:bg-slate-100 hover:scale-105 transition ease-in-out duration-300">
+    <div className="relative -m-2 flex items-center space-x-4 rounded-xl p-2 focus-within:ring-2 focus-within:ring-indigo-500 hover:bg-slate-100 hover:scale-105 transition ease-in-out duration-300 dark:hover:bg-slate-700 group">
       <div className={iconClassName}>
         <Icon className="h-6 w-6 text-white" aria-hidden="true" />
       </div>
       <div>
-        <h4 className="text-sm font-medium text-slate-900 cursor-pointer">
+        <h4 className="text-sm font-medium text-slate-900 cursor-pointer dark:text-slate-200 dark:group-hover:text-slate-100">
           <button
             onClick={(event) =>
               handleCampaignTypeButtonClick !== undefined
@@ -102,7 +102,7 @@ export function CampaignType({
             <Arrow />
           </button>
         </h4>
-        <p className="mt-1 text-sm text-slate-600 cursor-pointer">
+        <p className="mt-1 text-sm text-slate-600 cursor-pointer dark:text-slate-400 dark:group-hover:text-slate-200">
           {description}
         </p>
       </div>
